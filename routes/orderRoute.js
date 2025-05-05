2d35ae92b130d045bcc657d958ff157806e240df
98534 <9853491653@mail.ru>
2025-05-06 00:25:47 +0300

    server start

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import { insertOrder } from '../controllers/insertOrder.js';
+import multer from 'multer';
 
 const router = express.Router();
-router.post('/order', insertOrder);
+const upload = multer(); // Можно настроить, если надо сохранять файлы
+
+router.post('/order', upload.none(), (req, res) => {
+  console.log('Body:', req.body);
+  // req.files — если будет upload.any() или upload.array()
+  res.status(200).json({ message: 'Form received successfully' });
+});
 
 export default router;
