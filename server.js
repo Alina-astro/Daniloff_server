d4e622833a4ee87807ffb7ac54b0f000a2e403cc
98534 <9853491653@mail.ru>
2025-05-05 23:12:54 +0300

    server start

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 import orderRoute from './routes/orderRoute.js';
 
 const app = express();
-const port = 3005;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
