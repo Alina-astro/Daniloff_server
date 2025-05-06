a5eb3094da0fa7cb24a7e4199754da56bd9644b5
98534 <9853491653@mail.ru>
2025-05-06 20:56:50 +0300

    исправлена ошибка отправки данных в бд

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import multer from 'multer';
+import { insertOrder } from '../controllers/insertOrder.js';
 
 const router = express.Router();
 const upload = multer(); // Можно настроить, если надо сохранять файлы
 
-router.post('/order', upload.none(), (req, res) => {
-  console.log('Body:', req.body);
-  // req.files — если будет upload.any() или upload.array()
-  res.status(200).json({ message: 'Form received successfully' });
-});
+// router.post('/order', upload.none(), (req, res) => {
+//   console.log('Body:', req.body);
+//   // req.files — если будет upload.any() или upload.array()
+//   res.status(200).json({ message: 'Form received successfully' });
+// });
+
+router.post('/order', upload.none(), insertOrder);
 
 export default router;
